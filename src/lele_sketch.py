6b5079577c843dacbeeed69c1bf7f685cf1161d9
lelluzzo99 <gabridomin99@gmail.com>
2021-03-11 19:48:58 +0100

    move antenna

--- src/lele_sketch.py
+++ src/lele_sketch.py
@@ -15,3 +15,36 @@ def cost(buildings, antennas, reward):
     if total_coverage:
         total_reward += reward
     return total_reward
+
+
+def move_antenna(grid, x, y, d):
+    if x - 2*d < 0:
+        x1 = 0
+    else:
+        x1 = x -2 * d
+    if x + 2 * d >= grid.shape[0]:
+        x2 = grid.shape[0]
+    else:
+        x2 = x + 2 * d
+    if y - 2*d < 0:
+        y1 = 0
+    else:
+        y1 = y -2 * d
+    if y + 2 * d >= grid.shape[0]:
+        y2 = grid.shape[0]
+    else:
+        y2 = y + 2 * d
+    cord = []
+    for indexr, r in enumerate(grid[x1:x2]):
+        for indexc, c in enumerate(r[y1:y2]):
+            if c[0] != -1 and c[1] == -1:
+                cord.append((indexr, indexc))
+    finalx = 0
+    finaly = 0
+    for build in cord:
+        finalx += build[0] - x
+        finaly += build[1] - y
+    finalx = x + finalx / len(cord)
+    finaly = y + finaly / len(cord)
+
+    return finalx, finaly
